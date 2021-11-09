3a9a6ac547669631d94885d76999bed7aba9c17f
Jan Provaznik <janpro@janpro.dev>
2021-11-09 11:21:07 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Proyecto de Fundamentos de Marketing
+## Va a scrape data de web sobre hoteles para analizarlo
---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import bs4
+
+def analyse():
+    # Use a breakpoint in the code line below to debug your script.
+    pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    pass
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
